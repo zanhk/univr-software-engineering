a477e02e828c438693de7d26128d34f5a1e4ce28
CesarStef <cesarstef@outlook.com>
2019-06-13 16:06:29 +0200

    Aggiunto file Use Case, aggiunti commenti per futuro java doc

--- src/main/java/dev/matteomeneghetti/sendhelp/data/CartellaClinica.java
+++ src/main/java/dev/matteomeneghetti/sendhelp/data/CartellaClinica.java
@@ -1,54 +1,136 @@
 package dev.matteomeneghetti.sendhelp.data;
 
 import java.util.TreeSet;
-/*
-    La cartella clinica e' il cuore pulsante della gestione dei dati.
-    Per ogni paziente in fase di ricovero viene aperta una cartella clinica,
-    nel quale sono inserite generalita' del paziente al momento del ricovero, 
-    in seguito possono venire aggiunte prescrizioni di medicinali 
-    e un record delle relative somministrazioni.
-    Al termine del ricovero, il primario compila una lettera di dimissioni 
-    riassuntiva di quanto avvenuto al paziente durante la permanenza e chiude in tal
-    modo la cartella clinica del paziente.
-*/
+/** 
+ *La cartella clinica e' il cuore pulsante della gestione dei dati.
+ *Per ogni paziente in fase di ricovero viene aperta una cartella clinica,
+ *nel quale sono inserite generalita' del paziente al momento del ricovero, 
+ *in seguito possono venire aggiunte prescrizioni di medicinali 
+ *e un record delle relative somministrazioni.
+ *Al termine del ricovero, il primario compila una lettera di dimissioni 
+ *riassuntiva di quanto avvenuto al paziente durante la permanenza e chiude in tal
+ *modo la cartella clinica del paziente.
+ */
 public class CartellaClinica {
     
     private Paziente paziente;
     private TreeSet<Prescrizione> prescrizioni;
     private TreeSet<Somministrazione> somministrazioni;
     
+    /**
+     *Costruttore per la classe Cartella Clinica
+     *@param paziente Necessario per costruire la cartella clinica
+     *@see Paziente
+     */
+     
     public CartellaClinica(Paziente paziente) {
         setPaziente(paziente);
         this.prescrizioni = new TreeSet<Prescrizione>();
         this.somministrazioni = new TreeSet<Somministrazione>();
     }
-    
+
+    /** 
+     * 
+     *Ritorna il paziente relativo a questa cartella clinica
+     *@return un oggetto di tipo paziente
+     *@see Paziente
+     *
+     */
+
     public Paziente getPaziente() {
         return this.paziente;
     }
+
+    /** 
+     * 
+     *Modifica il paziente per questa cartella clinica
+     *@param paziente Un oggetto di tipo paziente 
+     *@see Paziente
+     *
+     */
+
     public void setPaziente(Paziente paziente) {
         this.paziente = paziente;
     }
+
+     /** 
+     * 
+     *Ritorna un TreeSet con le prescrizioni
+     *@return tutte le prescrizioni di questa cartella clinica
+     *@see Prescrizione
+     *
+     */
     
     public TreeSet<Prescrizione> getPrescrizioni() {
         return this.prescrizioni;
     }    
-    public boolean addPrescrizione(Prescrizione nuovaPrescrizione) {
-        return this.prescrizioni.add(nuovaPrescrizione);
+
+     /** 
+     * 
+     *Aggiunge una prescrizione al TreeSet prescrizioni e
+     *ritorna un bool se è avvenuta correttamente
+     *@param prescrizione La prescrizione da aggiungere
+     *@return Un bool, TRUE se avvenuta, FALSE in caso contrario
+     *@see Prescrizione
+     *
+     */
+
+    public boolean addPrescrizione(Prescrizione prescrizione) {
+        return this.prescrizioni.add(prescrizione);
     }
-    public boolean removePrescrizione(Prescrizione prescrizioneDaTogliere) {
-        return this.prescrizioni.remove(prescrizioneDaTogliere);
+
+     /** 
+     * 
+     *Rimuove una prescrizione al TreeSet prescrizioni e
+     *ritorna un bool se è avvenuta correttamente
+     *@param prescrizione La prescrizione da togliere
+     *@return Un bool, TRUE se avvenuta, FALSE in caso contrario
+     *@see Prescrizione
+     *
+     */
+
+    public boolean removePrescrizione(Prescrizione prescrizione) {
+        return this.prescrizioni.remove(prescrizione);
     }    
-    
+
+    /** 
+     * 
+     *Ritorna un TreeSet con le somministrazioni
+     *@return tutte le somministrazioni di questa cartella clinica
+     *@see Somministrazione
+     *
+     */
     
     public TreeSet<Somministrazione> getSomministrazioni() {
         return this.somministrazioni;
     }
-    public boolean addSomministrazione(Somministrazione nuovaSomministrazione) {
-        return this.somministrazioni.add(nuovaSomministrazione);
+
+    /** 
+     * 
+     *Aggiunge una somministrazione al TreeSet somministrazioni e
+     *ritorna un bool se è avvenuta correttamente
+     *@param somministrazione La somministrazione da aggiungere
+     *@return Un bool, TRUE se avvenuta, FALSE in caso contrario
+     *@see Somministrazione
+     *
+     */
+
+    public boolean addSomministrazione(Somministrazione somministrazione) {
+        return this.somministrazioni.add(somministrazione);
     }
-    public boolean removeSomministrazione(Somministrazione somministrazioneDaTogliere) {
-        return this.somministrazioni.remove(somministrazioneDaTogliere);
+
+    /** 
+     * 
+     *Rimuove una somministrazione al TreeSet somministrazioni e
+     *ritorna un bool se è avvenuta correttamente
+     *@param somministrazione La somministrazione da togliere
+     *@return Un bool, TRUE se avvenuta, FALSE in caso contrario
+     *@see Somministrazione
+     *
+     */
+
+    public boolean removeSomministrazione(Somministrazione somministrazione) {
+        return this.somministrazioni.remove(somministrazione);
     }    
     
 }
